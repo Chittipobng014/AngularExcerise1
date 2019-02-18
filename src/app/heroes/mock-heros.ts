98d528d000474fb1470d06c465fbbedaff141653
Chittiphong <Army@Korawits-MacBook-Pro.local>
2019-02-18 11:47:44 +0800

    The heroes tutorial

---
+++ src/app/heroes/mock-heros.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {name: 'Saitama', id: 20},
+    {name: 'Reborn', id: 21},
+    {name: 'Naruto', id: 22},
+    {name: 'Ken', id: 23},
+    {name: 'Saito', id: 25},
+    {name: 'Otatsu', id: 10},
+]
